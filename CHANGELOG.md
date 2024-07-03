30e680e9709fc66a811d8b964465e3d7402684ec
Kovács Kristóf-Attila <kovacskristof200@gmail.com>
2024-07-03 19:49:06 +0300

    v 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+- Added support from dart 3.0.0
+
 ## 0.0.1
 
 - Initial version.
--- example/dart_future_pool_example.dart
+++ example/dart_future_pool_example.dart
@@ -1,6 +1,39 @@
+import 'dart:math';
 import 'package:future_pool/future_pool.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+Future<int> future() async {
+  int seconds = Random().nextInt(5) + 1;
+  await Future.delayed(Duration(seconds: seconds));
+
+  return seconds;
+}
+
+void main() async {
+  final results = await FuturePool.allSettled(
+    List<Future<int> Function()>.filled(10, () => future()),
+    maxConcurrency: 3,
+    // onFutureCompleted: (result, index) {
+    //   if (result.status == SettledStatus.fulfilled) {
+    //     print('Future $index fulfilled with value: ${result.value}');
+    //   } else {
+    //     print('Future $index rejected with reason: ${result.reason}');
+    //   }
+    // },
+    onFutureSuccess: (result, index) {
+      print('Future $index fulfilled with value: ${result.value}');
+    },
+    onFutureFail: (result, index) {
+      print('Future $index rejected with reason: ${result.reason}');
+    },
+  );
+
+  print('Finished all results $results');
+
+  for (var result in results) {
+    if (result.status == SettledStatus.fulfilled) {
+      print('Fulfilled: ${result.value}');
+    } else {
+      print('Rejected: ${result.reason}');
+    }
+  }
 }
